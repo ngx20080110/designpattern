83f514c9b7f385924aff59ba546b4378a29a619b
ngx20 <ngx20@DESKTOP-45DR61S.lan>
2017-07-14 21:56:36 +0800

    observer pattern
    subject
    observer

---
+++ src/ch2/observer/DisplayElement.java
@@ -0,0 +1,5 @@
+package ch2.observer;
+
+public interface DisplayElement {
+	public void display();
+}
---
+++ src/ch2/observer/Observer.java
@@ -0,0 +1,5 @@
+package ch2.observer;
+
+public interface Observer {
+	public void update(float temp, float humidity, float pressure);
+}
